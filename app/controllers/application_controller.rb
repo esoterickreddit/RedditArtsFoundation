99546d2b1ee8486fcf6c919615d42971ffda863f
Robert Lambert <rlambert85@gmail.com>
2010-05-29 02:03:20 -0400

    added logging, added filter for password > log...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  before_filter :check_authorization, :check_authentication
+  before_filter :check_authentication, :check_authorization
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-private
+protected
   def check_authentication
+    logger.debug "Checking Authentication"
     unless session[:user]
       session[:original_uri] = request.request_uri
       flash[:notice] = "Please signin"
@@ -16,6 +17,7 @@ private
   end
 
   def check_authorization
+    logger.debug "Checking Authorization"
     user = User.find(session[:user])
     unless user.roles.detect{|role|
       role.rights.detect{|right|
@@ -29,5 +31,5 @@ private
   end
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
 end
