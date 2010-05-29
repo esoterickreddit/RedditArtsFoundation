ffc7a946ed80196b090bc7e2834e7290c2fbddd6
Robert Lambert <rlambert85@gmail.com>
2010-05-29 00:52:20 -0400

    added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  before_filter :check_authorization, :check_authentication
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
@@ -11,6 +12,19 @@ private
       session[:original_uri] = request.request_uri
       flash[:notice] = "Please signin"
       redirect_to :action => "index", :controller => "browse"
+    end
+  end
+
+  def check_authorization
+    user = User.find(session[:user])
+    unless user.roles.detect{|role|
+      role.rights.detect{|right|
+        right.action == action_name && right.controller == controller_name
+        }
+      }
+      flash[:notice] = "You are not authorized to view the page you requested"
+      request.env["HTTP_REFERER"] ? (redirect_to :back) : (redirect_to :controller => "browse", :action => "hot")
+      return false
     end
   end
 
